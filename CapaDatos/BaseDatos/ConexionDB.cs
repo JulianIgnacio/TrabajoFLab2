fc49bdff7864977a7e9d45634b27d7794a811a6f
JulianIgnacio <montenegrojulianig@gmail.com>
2023-11-28 23:59:12 -0300

    inicializada coneccion base de datos

---
+++ CapaDatos/BaseDatos/ConexionDB.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CapaDatos.BaseDatos
+{
+    public class ConexionDB
+    {
+        public SqlConnection _conexionDB = new SqlConnection(DatosDB.CadenaConexionConSA);
+
+        public SqlConnection AbrirConexion()
+        {
+            try
+            {
+                if(_conexionDB.State == System.Data.ConnectionState.Closed) _conexionDB.Open();
+                return _conexionDB;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show($"Error en AbrirConexion: {ex.Message}");
+                return null;
+                
+            }
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            try
+            {
+                if (_conexionDB.State == System.Data.ConnectionState.Open) _conexionDB.Close();
+
+                return _conexionDB;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show($"Error en cierre Conexion :{ex.Message}");
+                return null;
+            }
+        }
+
+        public SqlConnection EstadoConexion()
+        {
+            MessageBox.Show($"Conexion Exitosa; El estado de la conexion es : {_conexionDB.State}","Test de la conexion",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            return _conexionDB;
+        }
+        
+    }
+}
---
+++ CapaDatos/BaseDatos/DatosDB.cs
@@ -0,0 +1,14 @@
+﻿namespace CapaDatos.BaseDatos
+{
+    internal class DatosDB
+    {
+        private const string Servidor = "JULIAN";
+        private const string BaseDatos = "Panaderia";
+        private const string Usuario = "sa";
+        private const string Password = "w/ss/m29";
+
+        public static string CadenaConexionConSA = $"Server ={Servidor};Database={BaseDatos};User={Usuario};Password={Password}";
+
+        public static string CadenaConexionsinSA = $"Data Source = {Servidor};Initial Catalog={BaseDatos};Integrated Security = True";
+    }
+}
